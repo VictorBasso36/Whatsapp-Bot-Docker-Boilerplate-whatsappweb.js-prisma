2daf883dc42b70e7be01a80cb24faf6693cb6afd
root <root@DESKTOP-SKBK36U>
2024-04-06 18:42:50 -0300

    fix docker image and web cache controll

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const qrcode = require('qrcode-terminal')
 
 const { Client, LocalAuth } = require('whatsapp-web.js')
+const wwebVersion = '2.2407.3';
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']},  
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    },
+
+    puppeteer: { 
+        headless: true,
+        executablePath: '/usr/bin/google-chrome',
+        args: ['--disable-gpu', '--no-sandbox'],
+
+    
+    },  
 });
 
 
